0dd032bca6570f9114b961accc82fb1765961672
Gonzalo Gonzalez <107937050+gg161087@users.noreply.github.com>
2022-11-06 23:05:15 -0300

    catalogo 1.2

--- ProyectoCine/catalogo/views.py
+++ ProyectoCine/catalogo/views.py
@@ -2,16 +2,16 @@ from django.shortcuts import render
 from .models import Genero, Pelicula, Director
 
 def index(request):
-    num_peliculas = Pelicula.all().count()
-    num_directores = Director.all().count()
-    num_generos = Genero.all().count()
+    num_peliculas = Pelicula.objects.all().count()
+    num_directores = Director.objects.all().count()
+    num_generos = Genero.objects.all().count()
 
-    render(
+    return render(
         request,
         'index.html',
         context={
-            'num_peliculas' = num_peliculas,
-            'num_directores' = num_directores,
-            'num_generos' = num_generos
-        }
+            'num_peliculas' : num_peliculas,
+            'num_directores' : num_directores,
+            'num_generos' : num_generos
+        }    
     )
